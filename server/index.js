d29bd38aee85f4ad203bb31b8b820808a881a9ef
deepakagashe18 <agashedeepak55@gmail.com>
2023-02-22 15:26:14 +0530

    done

--- server/index.js
+++ server/index.js
@@ -29,8 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 const DATABASE_URL = process.env.CONNECTION_URL;
 
-mongoose
-  .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => {
       console.log(`server running on port ${PORT}`);
